a44b969c233bd4d2daa6ae0d806ae3e5a9cc5e8f
Olle Brask <ollstah@gmail.com>
2024-06-14 12:40:27 +0000

    Fix typo

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -83,7 +83,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
-    'corseheaders'
+    'corseheaders',
     
     'profiles',
     'posts',
